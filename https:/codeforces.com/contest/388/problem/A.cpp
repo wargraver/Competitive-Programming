43248d987e507c17463fc667e0607191cea15853
wargraver <utkarshkrsingh2091@gmail.com>
2020-06-07 10:51:43 +0530

    Create A.cpp

---
+++ https:/codeforces.com/contest/388/problem/A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+#define pa pair<int,int>
+#define vec vector<int>
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define init(x, a) memset(x, a, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1000000001;
+ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/input.txt", "r", stdin);
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/output.txt", "w", stdout);
+	//freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/err.txt", "w", stderr);
+#endif
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	ll a[n];
+	for (i = 0; i < n; i++) cin >> a[i];
+	sort(a, a + n);
+	ct = 1; ll cap = 0;
+	for (i = 0; i < n; i++) {
+		cap = i / ct;
+		if (a[i] < cap) ct++;
+	}
+	cout << ct << endl;
+	return 0;
+}
