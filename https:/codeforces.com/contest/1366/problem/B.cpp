8902113807cb3d2e05d17df9e971bfbc6023be1c
wargraver <utkarshkrsingh2091@gmail.com>
2020-06-15 19:05:13 +0530

    Create B.cpp

---
+++ https:/codeforces.com/contest/1366/problem/B.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+#define pa pair<int,int>
+#define vec vector<int>
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define init(x, a) memset(x, a, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1000000001;
+ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/input.txt", "r", stdin);
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/output.txt", "w", stdout);
+	//freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/err.txt", "w", stderr);
+#endif
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	cin >> t;
+	while (t--) {
+		ll x, low, high; ans = 0;
+		cin >> n >> x >> m;
+		low = x; high = x;
+		for (i = 0; i < m; i++) {
+			cin >> j >> k;
+			if (low <= k && high >= j) {
+				low = min(low, j);
+				high = max(high, k);
+			}
+		}
+		cout << (high - low) + 1;
+		cout << endl;
+	}
+	return 0;
+}
