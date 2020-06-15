6b01e2ebb74b66bb1c5f9176bac8b09eec86002f
wargraver <utkarshkrsingh2091@gmail.com>
2020-06-15 19:04:38 +0530

    Create A.cpp

---
+++ https:/codeforces.com/contest/1366/problem/A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+#define pa pair<int,int>
+#define vec vector<int>
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define init(x, a) memset(x, a, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1000000001;
+ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/input.txt", "r", stdin);
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/output.txt", "w", stdout);
+	//freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/err.txt", "w", stderr);
+#endif
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	cin >> t;
+	while (t--) {
+		cin >> i >> j;
+		ll n = min(i, j);
+		ll m = max(i, j);
+		if (n <= m / 2) cout << n << endl;
+		else cout << (i + j) / 3 << endl;
+
+	}
+	return 0;
+}
