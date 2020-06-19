3a7ce21611cb169d2b3acf61a98334896be42e1e
wargraver <utkarshkrsingh2091@gmail.com>
2020-06-19 23:56:58 +0530

    Create A.cpp

---
+++ https:/codeforces.com/contest/1003/problem/A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
+#define pa pair<ll,ll>
+#define vec vector<ll>
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define init(x, a) memset(x, a, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1e9 + 7;
+//ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/input.txt", "r", stdin);
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/output.txt", "w", stdout);
+	freopen("C:/Users/Utkarsh Kumar Singh/Desktop/ALL CODE/err.txt", "w", stderr);
+#endif
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	cin >> n;
+	map<ll, ll> dict;
+	for (i = 0; i < n; i++) {
+		cin >> k;
+		dict[k]++;
+	} ll max1 = 0;
+	for (auto it = dict.begin(); it != dict.end(); it++) {
+		max1 = max(max1, it->S);
+	}
+	cout << max1 << endl;
+	return 0;
+}
