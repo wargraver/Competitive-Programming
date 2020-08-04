0dcd7dfab49c5a3ede4972a5ff0cc5dc01a11db7
Utkarsh Kumar Singh <utkarshkrsingh2091@gmail.com>
2020-08-04 18:33:45 +0530

    Create stair_climbing_memoization.cpp

---
+++ Dynamic Programming/stair_climbing_memoization.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
+#define pa pair<ll,ll>
+#define vec vector<ll>
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define F first
+#define S second
+//memset(x, 0, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1e9 + 7;
+ll max1 = (ll)1e9;
+ll a[100005] = {0};
+ll ways(ll n) {
+	if (n <= 1) return n;
+	else if (a[n] == 0) a[n] = ways(n - 1) + ways(n - 2);
+	return a[n];
+}
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("err.txt", "w", stderr);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		cout << ways(n) << endl;
+	}
+	return 0;
+}
