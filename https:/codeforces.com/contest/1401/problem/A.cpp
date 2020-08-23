f9df9a73f17d5045f02eba08b3da466d90c3748b
Utkarsh Kumar Singh <utkarshkrsingh2091@gmail.com>
2020-08-24 00:48:53 +0530

    Create A.cpp

---
+++ https:/codeforces.com/contest/1401/problem/A.cpp
@@ -0,0 +1,14 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    main(){
+    long long int a,n,k,t;
+    cin>>t;
+    while(t--){
+    cin>>n>>k;
+    if(k>=n)cout<<(k-n)<<endl;
+    else{ cout<<(n-k)%2<<endl; }
+    }
+     
+     
+     
+    }
