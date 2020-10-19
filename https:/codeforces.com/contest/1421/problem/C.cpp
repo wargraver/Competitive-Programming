2ab3d0eab5072f59381dbd410311564f343deca0
Utkarsh Kumar Singh <utkarshkrsingh2091@gmail.com>
2020-10-19 16:16:51 +0530

    Create C.cpp

---
+++ https:/codeforces.com/contest/1421/problem/C.cpp
@@ -0,0 +1,37 @@
+//https://codeforces.com/contest/1421/problem/C
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
+#define pa pair<ll,ll>
+#define vec vector<ll>
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define F first
+#define S second
+//memset(x, 0, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1e9 + 7;
+ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("err.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll i, j, k, n, m, ct = 0, t, ans = 0;
+    string str;
+    cin >> str;
+    n = str.size();
+    k = n - 2;
+    cout << 4 << endl;
+    cout << "R " << " " << 2 << endl;
+    cout << "L " << " " << 2 << endl;
+    cout << "R " << " " << 2 << endl;
+    cout << "R " << " " << 4 * n - 4 - 1 << endl;
+    return 0;
+}
