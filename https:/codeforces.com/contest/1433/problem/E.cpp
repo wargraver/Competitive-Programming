cc227f98aee9e5b6f50039cfd6b33b61832e963d
Utkarsh Kumar Singh <utkarshkrsingh2091@gmail.com>
2020-10-20 22:38:50 +0530

    Create E.cpp

---
+++ https:/codeforces.com/contest/1433/problem/E.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
+#define pa pair<ll,ll>
+#define vec vector<ll>
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define F first
+#define S second
+//memset(x, 0, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1e9 + 7;
+ll max1 = (ll)1e9;
+ll a[75][75];
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("err.txt", "w", stderr);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll i, j, k, n, m, ct = 0, t;
+
+	cin >> n;
+	ll ans = 1;
+	for ( i = 2; i < n; i++) {
+		if (i != n / 2) {
+			ans = ans * i;
+		}
+
+	}
+
+	cout << ans << endl;
+	return 0;
+}
