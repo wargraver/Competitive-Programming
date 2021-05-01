0f3e1a3f18cd86780f18ef9c36eb3e272f74f9fd
Utkarsh Kumar Singh <utkarshkrsingh2091@gmail.com>
2021-05-01 22:39:51 +0530

    Create zone2021_a.cpp

---
+++ https:/atcoder.jp/contests/zone2021/tasks/zone2021_a.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
+#define pa pair<ll,ll>
+#define vec vector<ll>
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define F first
+#define S second
+//memset(x, 0, sizeof(x))
+ll inf = (ll)1e18;
+ll mod = 1e9 + 7;
+ll max1 = (ll)1e9;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("err.txt", "w", stderr);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll i, j, k, n, m, ct = 0, t, ans = 0;
+	string str;
+	cin >> str;
+	for (i = 0; i < str.size() - 3; i++) {
+		if (str[i] == 'Z' && str[i + 1] == 'O' && str[i + 2] == 'N' && str[i + 3] == 'e') ct++;
+	}
+	cout << ct << endl;
+
+
+	return 0;
+}
